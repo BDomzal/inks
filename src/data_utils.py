1050e2f08cce6e64daf9e7adf98c2fa0efc7919a
BDomzal <basiadomzal@gmail.com>
2025-10-14 15:15:13 +0200

    Started rewriting Konstytucja_closest_class_assignment notebook using functions

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
+import re
 
 class InksDataset(Dataset):
     def __init__(self, X, y):
@@ -79,6 +80,13 @@ def set_negative_to_zero(inDKs_df, elements_to_keep):
         inDKs_df[columns_to_keep_inks + columns_to_keep_inds] = inDKs_df[columns_to_keep_inks + columns_to_keep_inds].clip(lower=0)
     return inDKs_df
 
+def set_negative_to_zero_v2(inDKs_df, elements_to_keep):
+    inDKs_df = inDKs_df.copy()
+    any_negative = (inDKs_df[elements_to_keep] < 0).sum().sum() == 0
+    if any_negative:
+        inDKs_df[elements_to_keep] = inDKs_df[elements_to_keep].clip(lower=0)
+    return inDKs_df
+
 def multiply_by_weights(inDKs_df, elements_to_keep, 
                         weights=[1, 1, 1, 10, 19, 20, 17, 9, 20, 1]):
     inDKs_df = inDKs_df.copy()
@@ -91,6 +99,16 @@ def multiply_by_weights(inDKs_df, elements_to_keep,
         inDKs_df[col] = weights[i]*inDKs_df[col]
     return inDKs_df
 
+def multiply_by_weights_v2(inDKs_df, elements_to_keep, 
+                        weights=[1, 1, 1, 10, 19, 20, 17, 9, 20, 1]):
+    inDKs_df = inDKs_df.copy()
+    weights = [el/sum(weights) for el in weights]
+
+    for i, col in enumerate(elements_to_keep):
+        inDKs_df[col] = weights[i]*inDKs_df[col]
+
+    return inDKs_df
+
 def normalize_to_Fe(inDKs_df, elements_to_keep):
     inDKs_df = inDKs_df.copy()
     columns_to_keep_inds = [el + '_i' for el in elements_to_keep]
@@ -100,6 +118,13 @@ def normalize_to_Fe(inDKs_df, elements_to_keep):
     for i, col in enumerate(columns_to_keep_inks):
         inDKs_df[col] = inDKs_df[col] / inDKs_df['Fe_a']
     inDKs_df = inDKs_df.drop(columns=['Fe_a', 'Fe_i'])
+    return inDKs_df
+
+def normalize_to_Fe_v2(inDKs_df, elements_to_keep):
+    inDKs_df = inDKs_df.copy()
+    for i, col in enumerate(elements_to_keep):
+        inDKs_df[col] = inDKs_df[col] / inDKs_df['Fe']
+    inDKs_df = inDKs_df.drop(columns=['Fe'])
     return inDKs_df
 
 def create_partition(inDKs_df):
@@ -239,3 +264,76 @@ def visualise_train_val_test_distributions(train_data,
 
     if path_to_save is not None:
         plt.savefig(path_to_save, bbox_inches='tight')
+
+def load_classes_description_df(classes_description_path):
+
+    classes_df = pd.read_excel(classes_description_path, header=1, usecols=['NAZWA', 'OPIS'])
+    classes_df['NAZWA_short'] = classes_df['NAZWA'].apply(lambda x: 
+                                                        re.split(r'(\d+)', x)[0] + re.split(r'(\d+)', x)[1] if len(re.split(r'(\d+)', x))>1 else re.split(r'(\d+)', x)[0]
+                                                       )                                            
+    classes_df.drop_duplicates(['NAZWA', 'OPIS'], inplace=True)
+    classes_df_supp = classes_df.drop_duplicates(['NAZWA_short']).copy()
+    classes_df_supp['NAZWA'] = classes_df['NAZWA_short']
+    classes_df = pd.concat([classes_df, classes_df_supp])
+    classes_df.drop_duplicates(inplace=True)
+    classes_df.reset_index(inplace=True, drop=True)
+    return classes_df
+
+
+def create_ground_truth_df_for_target_data(which_dataset, 
+                                            target_path, 
+                                            xrf_path, 
+                                            classes_description_df, 
+                                            how_many_outer_to_remove=0):
+
+    assert (which_dataset == 'Konstytucja_indicators' or which_dataset == 'Konstytucja_prediction' or which_dataset == 'XRF'), 'Invalid dataset name.'
+
+    def remove_outer(group, n):
+        if n == 0:
+            return group
+        else:
+            return group.iloc[n:-n] if len(group) > 2*n else pd.DataFrame(columns=group.columns)
+
+    if which_dataset == 'Konstytucja_indicators' or which_dataset == 'Konstytucja_prediction':
+    
+        ground_truth_df = pd.read_csv(target_path, usecols=['name'])
+        ground_truth_df['Sample_id'] = ground_truth_df['name'].apply(lambda x: x.split('_')[0] if len(x.split('_'))==2 else x.split('_')[0] + x.split('_')[1])
+        ground_truth_df['Sample_id'] = ground_truth_df['Sample_id'].apply(lambda x: x.replace('.', ''))
+        ground_truth_df['short'] = ground_truth_df['Sample_id'].apply(lambda x: re.split(r'\d+', x)[0])
+        ground_truth_df.drop(columns=['name'], inplace=True) 
+        ground_truth_df.reset_index(drop=True, inplace=True)
+        
+        if how_many_outer_to_remove>0:
+            ground_truth_df = ground_truth_df.groupby('Sample_id', group_keys=False).apply(remove_outer, n=how_many_outer_to_remove)
+
+    elif which_dataset == 'XRF':
+
+        ground_truth_df = pd.read_csv(xrf_path, usecols=['name'])
+        ground_truth_df.rename(columns={'name': 'Sample_id'}, inplace=True)
+        ground_truth_df['short'] = ground_truth_df['Sample_id'].apply(lambda x: re.split(r'\d+', x)[0])
+
+    ground_truth_df = pd.merge(left=ground_truth_df, right=classes_description_df, how='left', left_on='Sample_id', right_on='NAZWA')
+    
+    ground_truth_df['OPIS'] = ground_truth_df['OPIS'].apply(lambda x: str(x).strip())
+    ground_truth_df.drop(columns=['NAZWA', 'NAZWA_short'], inplace=True)
+    ground_truth_df.reset_index(drop=True, inplace=True)
+
+    return ground_truth_df
+
+
+def select_book(df, ground_truth_df, book_name):
+
+    assert df.shape[0] == ground_truth_df.shape[0], 'df and ground_truth_df have different lengths!'
+
+    if book_name == 'app':
+        df = df[ground_truth_df['short'].apply(lambda x: x.startswith('APP'))].copy()
+    elif book_name == 'asc':
+        df = df[ground_truth_df['short'].apply(lambda x: x.startswith('ASC'))].copy()
+    elif book_name == 'ml':
+        df = df[ground_truth_df['short'].apply(lambda x: x.startswith('ML'))].copy()
+    elif book_name == 'all':
+        pass
+
+    return df
+
+
