269c254c70d920e7c6e7a1bc35029fe95eeb365f
BDomzal <basiadomzal@gmail.com>
2025-10-22 17:26:50 +0200

    Added argument random_state to create_partition function

--- src/data_utils.py
+++ src/data_utils.py
@@ -128,10 +128,10 @@ def normalize_to_Fe_v2(inDKs_df, elements_to_keep):
     inDKs_df = inDKs_df.drop(columns=['Fe'])
     return inDKs_df
 
-def create_partition(inDKs_df):
+def create_partition(inDKs_df, random_state=1):
     indices = list(range(int(inDKs_df.shape[0])))
-    ind_train_all, ind_test_all = train_test_split(indices, test_size=0.2, random_state=1)
-    ind_train_all, ind_val_all = train_test_split(ind_train_all, test_size=0.25, random_state=1)
+    ind_train_all, ind_test_all = train_test_split(indices, test_size=0.2, random_state=random_state)
+    ind_train_all, ind_val_all = train_test_split(ind_train_all, test_size=0.25, random_state=random_state)
 
     partition = {'train': ind_train_all,
             'val': ind_val_all,
