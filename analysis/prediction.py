d1096ceeb15624025054ee7218d24cd631448bec
BDomzal <basiadomzal@gmail.com>
2026-02-09 16:43:52 +0100

    Updated prediction after sorting rows in preprocessed.csv files (Kopernik, Merkuriusz, Konstytucja)

--- analysis/prediction.py
+++ analysis/prediction.py
@@ -9,7 +9,7 @@ import json
 with open('../config.json', 'r') as f:
     config = json.load(f)
 
-DATASET = "Kopernik"
+DATASET = "Konstytucja"
 
 PREPROCESSING_METHOD = config["preprocessing_method"] # normalization / logarithm
 HOW_MANY_OUTER_TO_REMOVE = config["how_many_outer_to_remove"]
