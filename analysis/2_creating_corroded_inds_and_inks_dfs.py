367d7783cc88f4c977db57996cfa7f791f139ae7
BDomzal <basiadomzal@gmail.com>
2026-02-25 14:58:33 +0100

    Added preprocessed data for inks only (model inks)

--- analysis/2_creating_corroded_inds_and_inks_dfs.py
+++ analysis/2_creating_corroded_inds_and_inks_dfs.py
@@ -8,6 +8,7 @@ with open('../config.json', 'r') as f:
     config = json.load(f)
 
 DATASET = "training"
+#DATASET = "model_inks"
 
 PREPROCESSING_METHOD = config["preprocessing_method"] # normalization / logarithm
 HOW_MANY_OUTER_TO_REMOVE = config["how_many_outer_to_remove"]
@@ -16,7 +17,8 @@ ELEMENTS_TO_KEEP = config["elements_to_keep"]
 MULTIPLICATION_WEIGHTS = config["multiplication_weights"]
 CORRODED = config["corroded"]
 
-PREPROCESSED_DATA_PATH = config["preprocessed_data_path"][DATASET.split('_')[0]]
+PREPROCESSED_DATA_PATH = config["preprocessed_data_path"][DATASET]
+INKS_ONLY_PATH = config["inks_only_path"][DATASET]
 
 
 inds_df, inks_df, sample_order = prepare_data_without_splitting(
@@ -25,18 +27,24 @@ inds_df, inks_df, sample_order = prepare_data_without_splitting(
                                                                 ELEMENTS_TO_KEEP,
                                                                 MULTIPLICATION_WEIGHTS,
                                                                 PREPROCESSING_METHOD,
-                                                                indicators_suffix='_i', 
+                                                                indicators_suffix='_i',
                                                                 inks_suffix='_a',
                                                                 normalisation_to_Fe=NORMALISATION_TO_FE
                                                                 )
 
 if NORMALISATION_TO_FE:
     ELEMENTS_TO_KEEP_NO_FE = [el for el in ELEMENTS_TO_KEEP if el != 'Fe']
-    corroded_inds_df = inds_df[inds_df['name'].apply(lambda x: sample_in_list(x, CORRODED))][ELEMENTS_TO_KEEP_NO_FE + ['name']]
-    corroded_inks_df = inks_df[inks_df['name'].apply(lambda x: sample_in_list(x, CORRODED))][ELEMENTS_TO_KEEP_NO_FE]
+    if DATASET == "training":
+        inds_df = inds_df[inds_df['name'].apply(lambda x: sample_in_list(x, CORRODED))]
+        inks_df = inks_df[inks_df['name'].apply(lambda x: sample_in_list(x, CORRODED))]
+    inds_df = inds_df[ELEMENTS_TO_KEEP_NO_FE + ['name']]
+    inks_df = inks_df[ELEMENTS_TO_KEEP_NO_FE]
 else:
-    corroded_inds_df = inds_df[inds_df['name'].apply(lambda x: sample_in_list(x, CORRODED))][ELEMENTS_TO_KEEP + ['name']]
-    corroded_inks_df = inks_df[inks_df['name'].apply(lambda x: sample_in_list(x, CORRODED))][ELEMENTS_TO_KEEP]
+    if DATASET == "training":
+        inds_df = inds_df[inds_df['name'].apply(lambda x: sample_in_list(x, CORRODED))]
+        inks_df = inks_df[inks_df['name'].apply(lambda x: sample_in_list(x, CORRODED))]
+    inds_df = inds_df[ELEMENTS_TO_KEEP + ['name']]
+    inks_df = inks_df[ELEMENTS_TO_KEEP]
 
-#corroded_inds_df.to_csv('../data/corroded/nn_ready/corroded_inds.csv', index=False)
-corroded_inks_df.to_csv('../results/corroded/corroded_inks.csv', index=False, header=False)
+#inds_df.to_csv('../data/corroded/nn_ready/corroded_inds.csv', index=False)
+inks_df.to_csv(INKS_ONLY_PATH, index=False, header=False)
